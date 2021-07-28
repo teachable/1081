8818f14184e36f3b3300c01ffded7e80fbfaefac
teachable <87560308+teachable@users.noreply.github.com>
2021-07-29 00:09:57 +0530

    Updating

---
+++ Challenge.java
@@ -0,0 +1,29 @@
+
+ class InstanceCounter {
+    private static int numInstances = 0;
+
+    protected static int getCount() {
+        return numInstances;
+    }
+
+    private static void addInstance() {
+        numInstances++;
+    }
+
+    InstanceCounter() {
+        InstanceCounter.addInstance();
+    }
+
+  
+}
+class Challenge {
+    public static void main(String[] arguments) {
+        System.out.println("Starting with " +
+            InstanceCounter.getCount() + " objects");
+        for (int  i = 0; i < 500; ++i) {
+            new InstanceCounter();
+        }
+        System.out.println("Created " +
+            InstanceCounter.getCount() + " objects");
+    }
+}
